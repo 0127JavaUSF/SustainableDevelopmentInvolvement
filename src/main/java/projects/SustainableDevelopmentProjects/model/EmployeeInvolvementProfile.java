7c16892a8902ebb33541c20001d16b99980a548f
Jean - Louis Macle <jeanlouis.macle@gmail.com>
2020-07-02 01:16:07 -0400

    Fixed a git revert issue

---
+++ src/main/java/projects/SustainableDevelopmentProjects/model/EmployeeInvolvementProfile.java
@@ -0,0 +1,100 @@
+package projects.SustainableDevelopmentProjects.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name="SD_Employee_Profile")
+public class EmployeeInvolvementProfile {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="SD_Employee_Id")
+	@NotNull
+	private long id;
+	
+	@Column(name="SD_First_Name", length=80)
+	@Size(max=80, message="First name should not be longer than 80 characters.")
+	private String firstName;
+	
+	@Column(name="SD_Last_Name", length=80)
+	@Size(max=80, message="Last name should not be longer than 80 characters.")
+	private String lastName;
+	
+	@JoinColumn(name="project_id", referencedColumnName = "SD_Project_Id")
+	@Column(name="project_id")
+	@NotNull
+	private Long project_Id;
+	
+	public EmployeeInvolvementProfile(long id,
+			@Size(max = 80, message = "First name should not be longer than 80 characters.") String firstName,
+			@Size(max = 80, message = "Last name should not be longer than 80 characters.") String lastName,
+			Long project_Id) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.project_Id = project_Id;
+	}
+
+
+	public EmployeeInvolvementProfile() {}
+
+
+	public long getId() {
+		return id;
+	}
+
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+
+	public String getLastName() {
+		return lastName;
+	}
+
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+
+	public Long getProject_Id() {
+		return project_Id;
+	}
+
+
+	public void setProject_Id(Long project_Id) {
+		this.project_Id = project_Id;
+	}
+
+
+	@Override
+	public String toString() {
+		return "EmployeeInvolvementProfile [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName
+				+ ", project_Id=" + project_Id + "]";
+	}
+	
+	
+	
+
+}
---
+++ src/main/java/projects/SustainableDevelopmentProjects/model/repository/EmployeeInvolvementProfileRepository.java
@@ -0,0 +1,9 @@
+package projects.SustainableDevelopmentProjects.model.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import projects.SustainableDevelopmentProjects.model.EmployeeInvolvementProfile;
+
+public interface EmployeeInvolvementProfileRepository extends JpaRepository<EmployeeInvolvementProfile, Long> {
+
+}
